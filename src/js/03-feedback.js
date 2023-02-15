407d22d356256c47e5840f4a5c93e020cf550091
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-02-15 22:14:02 +0200

    feedback done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import lodashThrottle from 'lodash.throttle';
+import storage from './storage';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const formData = {
+    email: '',
+    message: '',
+};
+
+if (localStorage.length > 0) {
+    formData.email = storage.load('feedback-form-state').email;
+    formData.message = storage.load('feedback-form-state').message;
+    feedbackForm.email.value = storage.load('feedback-form-state').email;
+    feedbackForm.message.value = storage.load('feedback-form-state').message;
+}
+
+const onInput = e => {
+    e.target.value === ''
+        ? (formData[e.target.name] = null)
+        : (formData[e.target.name] = e.target.value);
+
+    storage.save('feedback-form-state', formData);
+};
+
+feedbackForm.addEventListener('input', lodashThrottle(onInput, 500));
+
+const onFormSubmit = e => {
+    e.preventDefault();
+
+    formData[e.target.name] = e.target.value;
+
+    console.log(formData);
+    e.currentTarget.reset();
+    localStorage.clear();
+};
+
+feedbackForm.addEventListener('submit', onFormSubmit);
